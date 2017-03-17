ec6fc750e6d73705373992e64bcbede36494a061
Jufebrown <jufebrown@gmail.com>
2017-03-17 15:43:53 -0500

    tests for popular products

---
+++ test/popularProducts-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { assert: {isArray} } = require("chai");
+const { popularProducts } = require("../lib/popularProducts.js");
+
+describe('popularProducts', () => {
+
+  it(`should return an array`, () => {
+    return popularProducts()
+    .then((data) => {
+      isArray(data)
+    })
+  })
+
+
+});
